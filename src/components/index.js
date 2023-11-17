fcbca116a9e91ba0dcf5fe3a4720e92012298c00
supunbb <125901930+supunbb@users.noreply.github.com>
2023-11-17 15:16:46 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+
+export{
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero
+};
